38666eb582c6162ac92a94506d0b5022192342be
Mouyassir Mounirou <mouyassirmounirou@gmail.com>
2019-10-04 17:16:09 +0200

    deuxieme commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ma calebasse aux mille saveurs</title>
+	<link rel="stylesheet" type="text/css" href="css/">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<p>Compte 1:
+			-login:emmanuel@gmail.com
+			-password:1234
+
+			Compte 2:
+			-login:mounirou@gmail.com
+			-password:1234
+
+			Compte 3:
+			-login:papa@gmail.com
+			-password:123456
+		</p>
+	</div>
+</body>
+</html>
